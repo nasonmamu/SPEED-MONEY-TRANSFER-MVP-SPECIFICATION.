6f13708e5dee3de1bbc5c320d62a34aa80a9c201
nasonmamu <nasonmamu94@gmail.com>
2024-06-10 10:56:55 +0300

    Contain the javascript file of the the website server

--- server.js/setup sever.js
+++ server.js/setup sever.js
@@ -1,38 +1,20 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const path = require('path');
-const connectDB = require('./config/db');
-const dotenv = require("dotenv").config();
-const errorHandler = require('./middleware/errorMiddleware');
-
 const app = express();
-connectDB();
 
-// Middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+const PORT = process.env.PORT || 3000;
+
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'views')));
 
-// Routes
-const authRoutes = require('./routes/auth');
-const transactionRoutes = require('./routes/transactions');
-const userRoutes = require('./routes/users');
-
-app.use('/api/auth', authRoutes);
-app.use('/api/transactions', transactionRoutes);
-app.use('/api/users', userRoutes);
-
-// Serve static files from the 'views' directory
-app.use('/', express.static(path.join(__dirname, 'views')));
-
-// Catch-all route for handling 404 errors
-app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html')); // Create a 404.html page if needed
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Error Handling middleware
-app.use(errorHandler);
+app.get('/logout.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'logout.html'));
+});
 
-// Start the server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
